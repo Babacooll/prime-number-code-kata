3ba6305e7a5d46a3d3c8830513804274f23d8c49
Michael Garrez <michael.garrez@doclerholding.com>
2017-04-07 08:49:44 +0200

    Add accuracy for Miller rabin test

--- src/Strategy/MillerRabinPrimeNumberStrategy.php
+++ src/Strategy/MillerRabinPrimeNumberStrategy.php
@@ -10,22 +10,37 @@ namespace OptimusPrime\Strategy;
 class MillerRabinPrimeNumberStrategy implements PrimeNumberStrategyInterface
 {
 	/**
+	 * @var int
+	 */
+	private $accuracy;
+
+	/**
+	 * MillerRabinPrimeNumberStrategy constructor.
+	 *
+	 * @param int $accuracy
+	 */
+	public function __construct($accuracy = 100)
+	{
+		$this->accuracy = $accuracy;
+	}
+
+	/**
 	 * @param float $number
 	 *
 	 * @return bool
 	 */
 	public function isPrimeNumber(float $number): bool
 	{
-		$k = 100;
-
 		if ($number == 2)
 		{
 			return true;
 		}
+
 		if ($number < 2 || $number % 2 == 0)
 		{
 			return false;
 		}
+
 		$d = $number - 1;
 		$s = 0;
 
@@ -34,21 +49,26 @@ class MillerRabinPrimeNumberStrategy implements PrimeNumberStrategyInterface
 			$d /= 2;
 			$s++;
 		}
-		for ($i = 0; $i < $k; $i++)
+
+		for ($i = 0; $i < $this->accuracy; $i++)
 		{
 			$a = rand(2, $number - 1);
 			$x = bcpowmod($a, $d, $number);
+
 			if ($x == 1 || $x == $number - 1)
 			{
 				continue;
 			}
+
 			for ($j = 1; $j < $s; $j++)
 			{
 				$x = @bcmod(bcmul($x, $x), $number);
+
 				if ($x == 1)
 				{
 					return false;
 				}
+
 				if ($x == $number - 1)
 				{
 					continue 2;
